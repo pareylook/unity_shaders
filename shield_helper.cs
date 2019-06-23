42987d82fe862b71a754e6bd5a4b6a0973c7421f
Evgeny Starostin <pareylook@gmail.com>
2019-06-23 10:00:32 +0300

    Update shield_helper.cs

--- shield_helper.cs
+++ shield_helper.cs
@@ -19,13 +19,34 @@ public class ShieldHelper : MonoBehaviour
 
     }
 
-    int Hit = 0;
+    int hit = 0;
     void OnCollisionEnter(Collision collision)
-    {           
+    {
+        //shieldMatetial.SetVector("_Point0", collision.contacts[0].point);
+        //shieldMatetial.SetFloat("_TimeImpact0", Time.timeSinceLevelLoad);
+        if (hit == 0)
+        {
+            shieldMatetial.SetVector("_Point0", collision.contacts[0].point);
+            shieldMatetial.SetFloat("_TimeImpact0", Time.timeSinceLevelLoad);
+            hit++;
+        }
+        else if (hit == 1)
+        {
+            shieldMatetial.SetVector("_Point1", collision.contacts[0].point);
+            shieldMatetial.SetFloat("_TimeImpact1", Time.timeSinceLevelLoad);
+            hit++;
+        }
+        else if (hit == 2)
+        {
+            shieldMatetial.SetVector("_Point2", collision.contacts[0].point);
+            shieldMatetial.SetFloat("_TimeImpact2", Time.timeSinceLevelLoad);
+            hit++;
+        }
 
-        shieldMatetial.SetVector("_Points", collision.contacts[0].point);
-        shieldMatetial.SetFloat("_TimeImpact", Time.timeSinceLevelLoad);
-
+        if (hit >= 3)
+        {
+            hit = 0;
+        }
         //Debug.Log(pos);
         // shieldMatetial.SetInt("_PointsSize", 0);
 
